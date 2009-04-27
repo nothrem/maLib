ac6c4d2b100f7b12cd81d477609b73286bd5b355
nothrem <malib@nothrem.cz>
2009-04-27 20:13:17 +0000

    + base object extended from Ext.util.Observable
    git-svn-id: svn://kyo.chobits.local/Source/maLib@13 461a165f-1561-e147-9348-f0621103d71f

---
+++ framework/Base.js
@@ -0,0 +1,75 @@
+/**
+ * @author Nothrem Sinsky <malib@nothrem.cz>
+ *
+ * This library is distributed as Open-Source.
+ * Whole library or any part of it can be downloaded
+ * from svn://chobits.ch/source/maLib and used for free.
+ *
+ * Author does not guarantee any support and takes no resposibility for any damage.
+ * You use this code at your own risk. You can modify it as long as this header is present and unchaged!
+ * 
+ * This library may contain whole, parts or modifications of third party files,
+ * libraries, frameworks or other code, which are published under one of 'free'
+ * licences. See head of main file or file LICENCE.* .
+ */
+
+/**
+ * Required parts:
+ *   Ext
+ *   Ext.util.Observable
+ * Optional parts:
+ *   NONE
+ */
+
+/**
+ * @constructor
+ * @extends Ext.util.Observable
+ * 
+ * Creates new base object for maFx
+ * 
+ * @param  [void]
+ * @return [void]
+ */
+ma.Base = function() {
+ 	ma.Base.superclass.constructor.apply(this, arguments);
+	this.isReady = true;
+	
+	this.addEvents({
+		
+	});
+};
+
+Ext.extend(ma.Base, Ext.util.Observable, {
+	/**
+	 * @scope ma.Base
+	 */
+	// static properties
+	_isMaFx: true,
+	_className: 'Base',
+	_fullName: 'ma.Base',
+	
+	/**
+	 * Merges this object with given values (deep merge)
+	 * 
+	 * @param  values [Object] object with properties
+	 * @return [void]
+	 */
+	merge: function(values) {
+		ma.util.merge(this, values);
+	},
+	/**
+	 * Creates new deep clone of this object
+	 * 
+	 * @param  values [Object] Values to change
+	 * @return [Object] new clone
+	 */
+	clone: function(values) {
+		return ma.util.clone(this, values);
+	}
+});
+
+Ext.apply(ma.Base, {
+	_className: '[class]Base',
+	_fullName: '[class]ma.Base'
+});
+
