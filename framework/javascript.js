3ca55cc494ab51dbc7aa67706445682afe64f62f
Nothrem Sinsky <git@nothrem.cz>
2013-02-22 11:14:28 +0100

    * changed file headers to contain GIT repository address

--- framework/javascript.js
+++ framework/javascript.js
@@ -2,8 +2,8 @@
  * @author Nothrem Sinsky <malib@nothrem.cz>
  *
  * This library is distributed as Open-Source.
- * Whole library or any part of it can be downloaded
- * from svn://svn.chobits.ch/source/maLib and used for free.
+ * Whole library or any part of it can be downloaded for free.
+ * GIT repository: ssh://git@jii.chobits.ch:17222/~/maLib (send your public key to git@chobits.ch to gain free access).
  *
  * Author does not guarantee any support and takes no resposibility for any damage.
  * You use this code at your own risk. You can modify it as long as this header is present and unchaged!
